f97e134bb2627f692b2280408877e92f7d3d992d
mybee <1144894155@qq.com>
2019-09-11 16:59:13 +0800

    添加Keys 用户自定义存储

--- spider/context.go
+++ spider/context.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"path"
 	"strings"
+	"time"
 
 	"github.com/gocolly/colly"
 	"github.com/mybee/go-spider/common"
@@ -25,6 +26,7 @@ const reqContexKey key = 0
 // Context gospider context of each callback
 type Context struct {
 	Task  *Task
+	Keys map[string]interface{}
 	c     *colly.Collector
 	nextC *colly.Collector
 
@@ -35,6 +37,122 @@ type Context struct {
 	// output
 	outputDB       *sql.DB
 	outputCSVFiles map[string]io.WriteCloser
+}
+
+/************************************/
+/******** METADATA MANAGEMENT********/
+/************************************/
+
+// Set is used to store a new key/value pair exclusively for this context.
+// It also lazy initializes  c.Keys if it was not used previously.
+func (ctx *Context) Set(key string, value interface{}) {
+	if ctx.Keys == nil {
+		ctx.Keys = make(map[string]interface{})
+	}
+	ctx.Keys[key] = value
+}
+
+// Get returns the value for the given key, ie: (value, true).
+// If the value does not exists it returns (nil, false)
+func (ctx *Context) Get(key string) (value interface{}, exists bool) {
+	value, exists = ctx.Keys[key]
+	return
+}
+
+// MustGet returns the value for the given key if it exists, otherwise it panics.
+func (ctx *Context) MustGet(key string) interface{} {
+	if value, exists := ctx.Get(key); exists {
+		return value
+	}
+	panic("Key \"" + key + "\" does not exist")
+}
+
+// GetString returns the value associated with the key as a string.
+func (ctx *Context) GetString(key string) (s string) {
+	if val, ok := ctx.Get(key); ok && val != nil {
+		s, _ = val.(string)
+	}
+	return
+}
+
+// GetBool returns the value associated with the key as a boolean.
+func (ctx *Context) GetBool(key string) (b bool) {
+	if val, ok := ctx.Get(key); ok && val != nil {
+		b, _ = val.(bool)
+	}
+	return
+}
+
+// GetInt returns the value associated with the key as an integer.
+func (ctx *Context) GetInt(key string) (i int) {
+	if val, ok := ctx.Get(key); ok && val != nil {
+		i, _ = val.(int)
+	}
+	return
+}
+
+// GetInt64 returns the value associated with the key as an integer.
+func (ctx *Context) GetInt64(key string) (i64 int64) {
+	if val, ok := ctx.Get(key); ok && val != nil {
+		i64, _ = val.(int64)
+	}
+	return
+}
+
+// GetFloat64 returns the value associated with the key as a float64.
+func (ctx *Context) GetFloat64(key string) (f64 float64) {
+	if val, ok := ctx.Get(key); ok && val != nil {
+		f64, _ = val.(float64)
+	}
+	return
+}
+
+// GetTime returns the value associated with the key as time.
+func (ctx *Context) GetTime(key string) (t time.Time) {
+	if val, ok := ctx.Get(key); ok && val != nil {
+		t, _ = val.(time.Time)
+	}
+	return
+}
+
+// GetDuration returns the value associated with the key as a duration.
+func (ctx *Context) GetDuration(key string) (d time.Duration) {
+	if val, ok := ctx.Get(key); ok && val != nil {
+		d, _ = val.(time.Duration)
+	}
+	return
+}
+
+// GetStringSlice returns the value associated with the key as a slice of strings.
+func (ctx *Context) GetStringSlice(key string) (ss []string) {
+	if val, ok := ctx.Get(key); ok && val != nil {
+		ss, _ = val.([]string)
+	}
+	return
+}
+
+// GetStringMap returns the value associated with the key as a map of interfaces.
+func (ctx *Context) GetStringMap(key string) (sm map[string]interface{}) {
+	if val, ok := ctx.Get(key); ok && val != nil {
+		sm, _ = val.(map[string]interface{})
+	}
+	return
+}
+
+// GetStringMapString returns the value associated with the key as a map of strings.
+func (ctx *Context) GetStringMapString(key string) (sms map[string]string) {
+	if val, ok := ctx.Get(key); ok && val != nil {
+		sms, _ = val.(map[string]string)
+	}
+	return
+}
+
+// GetStringMapStringSlice returns the value associated with the key as a map to a slice of strings.
+func (ctx *Context) GetStringMapStringSlice(key string) (smss map[string][]string) {
+	if val, ok := ctx.Get(key); ok && val != nil {
+		smss, _ = val.(map[string][]string)
+	}
+	return
 }
 
 func newContext(ctx context.Context, cancel context.CancelFunc, task *Task, c *colly.Collector, nextC *colly.Collector) (*Context, error) {
